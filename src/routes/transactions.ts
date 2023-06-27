d182651f40d967124f35fd8a52d30ad91139f983
Fernando Brino <fernandobrino11@gmail.com>
2023-06-27 13:55:49 -0300

    feat: create transactions route

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,12 +1,29 @@
 import { FastifyInstance } from "fastify";
 import { knex } from "../database";
+import { z } from "zod";
+import { randomUUID } from "node:crypto";
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get("/hello", async () => {
-    const transaction = await knex("transactions")
-      .where("amount", 1000)
-      .select("*");
+  app.post("/", async (request, reply) => {
+    // Validation schema for request body parameters
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(["credit", "debit"]),
+    });
 
-    return transaction;
+    // Validating if request body parameters match my schema
+    // The 'parse' function automatically throw an error if doesn't match my shema
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body
+    );
+
+    await knex("transactions").insert({
+      id: randomUUID(),
+      title,
+      amount: type === "credit" ? amount : amount * -1,
+    });
+
+    return reply.status(201).send();
   });
 }
