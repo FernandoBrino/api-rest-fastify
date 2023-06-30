09b462123ed6a951b7762c2c73b89ebb65c18663
Fernando Brino <fernandobrino11@gmail.com>
2023-06-30 12:12:23 -0300

    test: list transcations

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { test, beforeAll, afterAll, describe } from "vitest";
+import { beforeAll, afterAll, describe, it, expect } from "vitest";
 import request from "supertest";
 import { app } from "../src/app";
 
@@ -17,7 +17,7 @@ describe("Transactions routes", () => {
   // Description
   // What the test is supposed to do
   // Validation
-  test("user can create a new transaction", async () => {
+  it("should be able to create a new transaction", async () => {
     await request(app.server)
       .post("/transactions")
       .send({
@@ -28,5 +28,30 @@ describe("Transactions routes", () => {
       .expect(201);
 
     // expect(response.statusCode).toEqual(201); // Long sintax without using supertest lib
+  });
+
+  // Any test should be excluded of any context, a test can't be dependent of others tests
+  it("sould be able to list all transactions", async () => {
+    const createTransactionResponse = await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "New transaction",
+        amount: 5000,
+        type: "credit",
+      });
+
+    const cookies = createTransactionResponse.get("Set-Cookie");
+
+    const listTransactionsResponse = await request(app.server)
+      .get("/transactions")
+      .set("Cookie", cookies)
+      .expect(200);
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: "New transaction",
+        amount: 5000,
+      }),
+    ]);
   });
 });
