9fe8eac07456a87d7fe825aea438cab81a19d7c5
Fernando Brino <fernandobrino11@gmail.com>
2023-06-30 20:51:27 -0300

    feat: config to pg database and sqlite

--- src/env/index.ts
+++ src/env/index.ts
@@ -10,8 +10,9 @@ if (process.env.NODE_ENV === "test") {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
+  DATABASE_CLIENT: z.enum(["sqlite", "pg"]),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333), // coerce = trasnforms received value in number
 });
 
 // catch data inside 'process.env' and validate using the created schema 'envSchema';
